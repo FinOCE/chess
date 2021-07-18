4944fd9477abd29a03bd10a41c2cbb8dcdef2864
Finley Sherwood <me@itsf.in>
2021-07-18 20:08:56 +1000

    Fix react errors

--- components/board.tsx
+++ components/board.tsx
@@ -10,46 +10,33 @@ export default function board() {
 
     return (
         <div id={styles.board}>
-            {cells.map((a, x) => {
-                return (
-                    <>
-                        {a.map((b, y) => {
-                            let piece = board.pieces.find(piece => piece.position === `${Col[7-x]}${y+1}`)
+            {cells.map((a, x) => a.map((b, y) => {
+                let piece = board.pieces.find(piece => piece.position === `${Col[7-x]}${y+1}`)
 
-                            return (
-                                <div key={y} className={styles.checker} style={{background: colors[(x + y) % 2]}}>
-                                    {piece && (<span className={styles.icon}>
-                                        <Piece data={piece} />
-                                    </span>)}
-                                    {x === 7 ? (
-                                        <span className={[
-                                            styles.label,
-                                            styles.labely
-                                        ].join(' ')}>
-                                            {Col[y].toUpperCase()}
-                                        </span>
-                                    ) : ''}
-                                    {y === 0 ? (
-                                        <span className={[
-                                            styles.label,
-                                            styles.labelx
-                                        ].join(' ')}>
-                                            {8-x}
-                                        </span>
-                                    ) : ''}
-                                </div>
-                            )
-                        })}
-                    </>
-                )
-            })}
-            {/* {board.pieces.map((piece, i) => {
-                let top = (7 - (piece.position.charCodeAt(0) - 97)) * 100
-                let left = (Number(piece.position.split('')[1]) - 1) * 100
                 return (
-                    <Piece key={i} data={piece} style={{left, top}} />
+                    <div key={y} className={styles.checker} style={{background: colors[(x + y) % 2]}}>
+                        {piece && (<span className={styles.icon}>
+                            <Piece data={piece} />
+                        </span>)}
+                        {x === 7 ? (
+                            <span className={[
+                                styles.label,
+                                styles.labely
+                            ].join(' ')}>
+                                {Col[y].toUpperCase()}
+                            </span>
+                        ) : ''}
+                        {y === 0 ? (
+                            <span className={[
+                                styles.label,
+                                styles.labelx
+                            ].join(' ')}>
+                                {8-x}
+                            </span>
+                        ) : ''}
+                    </div>
                 )
-            })} */}
+            }))}
         </div>
     )
 }
