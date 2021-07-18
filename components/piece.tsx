f6ccc6075e01f6f99f8769c2d056ecbdebe0b0e5
Finley Sherwood <me@itsf.in>
2021-07-18 19:06:19 +1000

    Use fontawesome icons for pieces

--- components/piece.tsx
+++ components/piece.tsx
@@ -1,4 +1,6 @@
 import {Properties} from 'csstype'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faChessKing, faChessQueen, faChessBishop, faChessKnight, faChessRook, faChessPawn} from '@fortawesome/free-solid-svg-icons'
 import Piece from '../utils/Piece'
 import styles from '../styles/piece.module.sass'
 
@@ -8,9 +10,22 @@ export interface PieceProps {
 }
 
 export default function piece(props: PieceProps) {
+    let types = {
+        'King': faChessKing,
+        'Queen': faChessQueen,
+        'Bishop': faChessBishop,
+        'Knight': faChessKnight,
+        'Rook': faChessRook,
+        'Pawn': faChessPawn
+    }
+    let colors = {
+        'White': '#fff',
+        'Black': '#000'
+    }
+
     return (
-        <div className={styles.piece} style={props.style}>
-            {props.data.team} {props.data.type}
+        <div className={styles.piece} style={Object.assign(props.style, {color: colors[props.data.team]})}>
+            <FontAwesomeIcon icon={types[props.data.type]} />
         </div>
     )
 }
